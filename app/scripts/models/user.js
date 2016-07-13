a550c3214c28889ccd35aae7f4af127166f8ceb2
Matthew Rice <mtr.rice@gmail.com>
2016-07-13 15:58:44 -0400

    Changed url to https instead of http.

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -1,13 +1,13 @@
 var Backbone = require('backbone');
 
 var User = Backbone.Model.extend({
-  urlRoot: 'http://tiny-parse-server.herokuapp.com/users'
+  urlRoot: 'https://tiny-parse-server.herokuapp.com/users'
 },{
   login: function(username, password, callbacks){
     var loggedInUser = new User();
     var queryString = jQuery.param({'username': username, 'password': password});
 
-    loggedInUser.urlRoot = 'http://tiny-parse-server.herokuapp.com/login?' + queryString;
+    loggedInUser.urlRoot = 'https://tiny-parse-server.herokuapp.com/login?' + queryString;
 
     loggedInUser.fetch().done(function(data){
       localStorage.setItem('user', JSON.stringify(loggedInUser.toJSON()));
